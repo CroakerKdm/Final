10760d685a0ac1759fd502ae32ba49cd6854e68e
Croaker <CroakerKdm@gmail.com>
2021-04-19 20:40:18 +0300

    +

--- src/GLOSSARY/Main.java
+++ src/GLOSSARY/Main.java
@@ -3,9 +3,8 @@ package GLOSSARY;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 /**
@@ -37,9 +36,9 @@ public class Main {
         wordMap.entrySet().stream()
                 .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
                 .forEachOrdered(entry -> sorted.put(entry.getKey(), entry.getValue()));
-        sorted.entrySet().stream().limit(20).forEach(System.out::println);
 
-        StringBuilder sorted20 = new StringBuilder("_____________Sorted 20_____________\n");
+        StringBuilder sorted20 = new StringBuilder("_____________Sorted 20_____________\n")
+                .append("Nice header by Kyrylo Molchanov\n");
         int counter = 0;
         for (Map.Entry<String, Integer> entry : sorted.entrySet()) {
             counter++;
@@ -47,8 +46,67 @@ public class Main {
             if (counter == 20) break;
         }
 
+        List<String> list = Arrays.stream(words).collect(Collectors.toList());
+        List<String> properNamesList = list.stream().distinct().filter(entry -> !wordMap.containsKey(entry.toLowerCase()))
+                .filter(entry -> Character.isUpperCase(entry.charAt(0))).sorted().collect(Collectors.toList());
+
+        System.out.println("\nSORTED PAIRS");
+        sorted.entrySet().stream().limit(20).forEach(System.out::println);
         String fileOutput = "Distincts" + ".txt";
         Files.writeString(Paths.get(fileOutput), sorted20.toString());
+
+        System.out.println("\nSORTED NAMES");
+        System.out.println("Proper names number: " + properNamesList.size());
+        properNamesList.stream().limit(20).forEach(System.out::println);
+
     }
 }
+
+
+/*
+        SORTED PAIRS
+        the=3313
+        to=1845
+        and=1807
+        a=1578
+        of=1242
+        Harry=1213
+        was=1178
+        he=1033
+        in=933
+        his=896
+        it=804
+        said=793
+        you=735
+        had=697
+        I=652
+        on=617
+        at=581
+        that=580
+        him=495
+        He=494
+
+        SORTED NAMES
+        Proper names number: 670
+        AAAAAAAAAARGH
+        AAAARGH
+        ALBUS
+        ALLEY
+        Aaah
+        Aargh
+        Abbott
+        Absolutely
+        According
+        Adalbert
+        Adrian
+        Africa
+        African
+        Agrippa
+        Ah
+        Aha
+        Ahem
+        Ahern
+        Alas
+        Alberic
+*/
 
